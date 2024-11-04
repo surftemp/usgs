1003e7dc9c07228a37507d0ffc090173688ad6b3
Niall McCarroll <niall_mccarroll@yahoo.com>
2024-11-04 17:15:38 +0000

    support for auditing results of download

--- src/usgs/download/multithreaded_downloader.py
+++ src/usgs/download/multithreaded_downloader.py
@@ -1,5 +1,5 @@
 # Based on:
-
+import csv
 # ========================================================================================================================
 #  USGS/EROS Inventory Service Example
 #  Description: Download Landsat Collection 2 files
@@ -23,17 +23,32 @@ import re
 import threading
 import datetime
 import os
+import queue
 
 from usgs.utils.file_utils import FileUtils
 
+
+def create_download_path(download_folder, filename):
+    # for downloading to a separate cache folder, use year, month and day as subdirectories
+    # to avoid creating a single folder with a massive number of files
+    # split components to get the year and month
+    # example LC08_L2SP_009012_20180324_20200901_02_T1_MTL.xml
+    name_comps = filename.split("_")
+    year = name_comps[3][0:4]
+    month = name_comps[3][4:6]
+    day = name_comps[3][6:8]
+    download_folder_ymd = os.path.join(download_folder, year, month, day)
+    os.makedirs(download_folder_ymd, exist_ok=True)
+    return os.path.join(download_folder_ymd, filename)
+
 class MultiThreadedDownloader:
 
-    def __init__(self, file_cache_index_path=None, maxthreads=5, retry_delay=30):
-
+    def __init__(self, file_cache_index_path=None, maxthreads=5, retry_delay=30, retry_limit=5):
         self.maxthreads = maxthreads  # Threads count for downloads
-        self.sema = threading.Semaphore(value=maxthreads)
+        self.jobqueue = queue.Queue()
         self.threads = []
         self.retry_delay = retry_delay
+        self.retry_limit = retry_limit
         self.file_cache = FileUtils(file_cache_index_path) if file_cache_index_path else None
 
     # Send http request
@@ -89,36 +104,55 @@ class MultiThreadedDownloader:
         response.close()
         return output['data']
 
-    def download_file(self, url, download_folder, output_folder):
-        self.sema.acquire()
-        try:
-            response = requests.get(url, stream=True)
-            disposition = response.headers['content-disposition']
-            filename = re.findall("filename=(.+)", disposition)[0].strip("\"")
-            print(f"Downloading {filename} ...")
-            download_path = os.path.join(download_folder, filename)
-            output_path = os.path.join(output_folder, filename)
-            with open(download_path, 'wb') as f:
-                f.write(response.content)
-            print(f"Downloaded {filename}")
-            if download_path != output_path:
-                print("creating symlink to download: " + filename)
-                os.symlink(download_path,output_path)
+    def download_files(self):
+        while True:
+            work = self.jobqueue.get()
+            if work is not None:
+                completed = False
+                (url, download_folder, output_folder) = work
+                if "gap_mask" in url:
+                    # skip landsat7 gap masks
+                    print(f"Skipping {url} ...")
+                    completed = True
+                retry = 0
+                while not completed and retry <= self.retry_limit:
+                    try:
+                        print(f"Downloading from {url} ...")
+                        response = requests.get(url, stream=True)
+                        if not response.ok:
+                            print(
+                                f"Failed to download from {url}: {response.status_code}. Will try to re-download after a {self.retry_delay} second display.")
+                            retry += 1
+                            time.sleep(self.retry_delay)
+                            continue
+                        disposition = response.headers['content-disposition']
+                        filename = re.findall("filename=(.+)", disposition)[0].strip("\"")
+                        print(f"Downloading {filename} ...")
+                        if download_folder != output_folder:
+                            download_path = create_download_path(download_folder, filename)
+                        else:
+                            download_path = os.path.join(download_folder, filename)
+                        output_path = os.path.join(output_folder, filename)
+                        with open(download_path, 'wb') as f:
+                            f.write(response.content)
+                        print(f"Downloaded {filename}")
+                        if download_path != output_path:
+                            print("creating symlink to download: " + filename)
+                            os.symlink(download_path,output_path)
+                        completed = True
+                    except Exception as e:
+                        print(
+                            f"Failed to download from {url} due to exception: {str(e)}.")
+                        completed = True
+                if not completed:
+                    print(f"Failed to download from {url}. No retries left.")
+            else:
+                # when None is obtained from the queue, complete execution, there is no more work to do
+                break
 
-            self.sema.release()
-        except Exception as e:
-            print(
-                f"Failed to download from {url}: {str(e)}. Will try to re-download after a {self.retry_delay} second display.")
-            self.sema.release()
-            time.sleep(self.retry_delay)
-            self.run_download(url, download_folder, output_folder)
 
-    def run_download(self, url, download_folder, output_folder):
-        thread = threading.Thread(target=lambda *dargs: self.download_file(*dargs), args=(url, download_folder, output_folder))
-        self.threads.append(thread)
-        thread.start()
-
-    def fetch(self, username, password, scenefile, download_folder, output_folder, limit, suffixes, no_download=False):
+    def fetch(self, username, password, scenefile, download_folder, output_folder, limit, suffixes, no_download=False,
+              download_summary_path=""):
 
         with open(scenefile, "r") as f:
             lines = f.readlines()
@@ -130,43 +164,49 @@ class MultiThreadedDownloader:
         if output_folder != download_folder:
             os.makedirs(download_folder, exist_ok=True)
 
-        def include_file(display_id):
+        def require_file(display_id):
             required_suffix = False if suffixes else True
             filename = display_id
             if suffixes:
                 for ending in suffixes:
                     if filename.lower().endswith(ending.lower()):
-                        required_suffix = True
-
-            if not required_suffix:
+                        return True
                 return False
+            return True
 
-            download_path = os.path.join(download_folder, filename)
-            output_path = os.path.join(output_folder,filename)
+        def include_file_for_download(display_id):
+            filename = display_id
+            if download_folder != output_folder:
+                download_path = create_download_path(download_folder, filename)
+
+            else:
+                download_path = os.path.join(download_folder, filename)
+
+            output_path = os.path.join(output_folder, filename)
 
             if os.path.exists(output_path):
                 print("already in output: "+filename)
-                return False
+                return None
 
             if os.path.exists(download_path):
                 print("already downloaded: "+download_path)
                 if download_path != output_path:
                     print("creating symlink from download to output: "+filename)
                     os.symlink(download_path, output_path)
-                return False
+                return None
 
             if self.file_cache is not None:
                 cached_path = self.file_cache.get_path(filename)
                 if cached_path:
                     print("creating symlink from cache to output: " + filename)
                     os.symlink(cached_path, output_path)
-                    return False
+                    return None
 
             if no_download:
                 print(f"{filename}: download required but not enabled")
-                return False
+                return None
 
-            return True # this file needs to be downloaded
+            return download_path # this file needs to be downloaded
 
         label = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
@@ -200,24 +240,43 @@ class MultiThreadedDownloader:
 
         # Select products
         downloads = []
+        expected_outputs = []
 
         # Select band files
         scanned_files = set()
         for product in products:
+            product_entity_id = product["entityId"]
             if product["secondaryDownloads"] is not None and len(product["secondaryDownloads"]) > 0:
                 for secondaryDownload in product["secondaryDownloads"]:
                     if secondaryDownload["bulkAvailable"]:
                         display_id = secondaryDownload["displayId"]
+                        if not require_file(display_id):
+                            continue
                         if display_id in scanned_files:
                             continue
+                        expected_outputs.append([product_entity_id, display_id])
                         scanned_files.add(display_id)
-                        if include_file(display_id):
+                        if include_file_for_download(display_id):
                             downloads.append(
                                 {"entityId": secondaryDownload["entityId"], "productId": secondaryDownload["id"]})
+
+        if download_summary_path:
+            download_summary_folder = os.path.split(download_summary_path)[0]
+            os.makedirs(download_summary_folder,exist_ok=True)
+            with open(download_summary_path,"w") as f:
+                writer = csv.writer(f)
+                for expected_output in expected_outputs:
+                    writer.writerow(expected_output)
 
         if len(downloads) == 0:
             print("No downloads")
             sys.exit(0)
+
+        # start some worker threads
+        for i in range(0, self.maxthreads):
+            thread = threading.Thread(target=lambda *dargs: self.download_files())
+            thread.start()
+            self.threads.append(thread)
 
         payload = {
             "downloads": downloads,
@@ -230,7 +289,7 @@ class MultiThreadedDownloader:
 
         # Attempt the download URLs
         for result in results['availableDownloads']:
-            self.run_download(result['url'], download_folder, output_folder)
+            self.jobqueue.put((result['url'], download_folder, output_folder))
 
         preparingDownloadCount = len(results['preparingDownloads'])
         preparingDownloadIds = []
@@ -245,12 +304,12 @@ class MultiThreadedDownloader:
                 for result in results['available']:
                     if result['downloadId'] in preparingDownloadIds:
                         preparingDownloadIds.remove(result['downloadId'])
-                        self.run_download(result['url'], download_folder, output_folder)
+                        self.jobqueue.put((result['url'], download_folder, output_folder))
 
                 for result in results['requested']:
                     if result['downloadId'] in preparingDownloadIds:
                         preparingDownloadIds.remove(result['downloadId'])
-                        self.run_download(result['url'], download_folder, output_folder)
+                        self.jobqueue.put((result['url'], download_folder, output_folder))
 
             # Didn't get all download URLs, retrieve again after 30 seconds
             while len(preparingDownloadIds) > 0:
@@ -262,7 +321,7 @@ class MultiThreadedDownloader:
                     for result in results['available']:
                         if result['downloadId'] in preparingDownloadIds:
                             preparingDownloadIds.remove(result['downloadId'])
-                            self.run_download(result['url'], download_folder, output_folder)
+                            self.jobqueue.put((result['url'], download_folder, output_folder))
 
         print("\nGot download urls for all downloads\n")
 
@@ -272,6 +331,9 @@ class MultiThreadedDownloader:
             print("Logged Out\n")
         else:
             print("Logout Failed\n")
+
+        for i in range(self.maxthreads):
+            self.jobqueue.put(None) # this will cause the threads to terminate
 
         print("Downloading files... Please do not close the program\n")
         for thread in self.threads:
@@ -295,13 +357,14 @@ def main():
     parser.add_argument('-c', '--file-cache-index', type=str,
                         help='path to an key-value DBM index with filename->path cache lookup',default=None)
     parser.add_argument('-l', '--limit', type=int, help='limit to this many items', default=None)
+    parser.add_argument('-e', '--download-summary-path', help='path to write a CSV with summary of expected downloads', default='')
 
     args = parser.parse_args()
 
     dl = MultiThreadedDownloader(args.file_cache_index)
-    dl.fetch(args.username, args.password, args.filename,
-             args.download_folder, args.output_folder, args.limit,
-             args.file_suffixes, args.no_download)
+    dl.fetch(username=args.username, password=args.password, scenefile=args.filename,
+             download_folder=os.path.abspath(args.download_folder), output_folder=os.path.abspath(args.output_folder), limit=args.limit,
+             suffixes=args.file_suffixes, no_download=args.no_download, download_summary_path=args.download_summary_path)
 
 if __name__ == '__main__':
     main()
